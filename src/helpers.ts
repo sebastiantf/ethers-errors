187fb628203e982fc9485d9e8a55195d10c0c83e
Sebastian T F <sebastian.tharakan97@gmail.com>
2022-09-22 10:25:07 +0530

    feat(helpers): add `generateJSONSchema()` helper

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,4 +1,18 @@
+import { resolve } from 'path';
+import * as TJS from 'typescript-json-schema';
+
 export const EXECUTION_REVERTED = 'execution reverted: ';
 
 export const parseRevertReason = (message: string) =>
   message.slice(EXECUTION_REVERTED.length);
+
+// https://github.com/YousefED/typescript-json-schema#programmatic-use
+export const generateJSONSchema = (fileName: string, typeName: string) => {
+  const program = TJS.getProgramFromFiles([resolve(fileName)], {
+    strictNullChecks: true
+  });
+
+  return TJS.generateSchema(program, typeName, {
+    required: true
+  });
+};
