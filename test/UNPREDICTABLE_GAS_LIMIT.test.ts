d91362363c44f21c997fd1cd69710fd081970b5d
Sebastian T F <sebastian.tharakan97@gmail.com>
2022-09-22 10:27:57 +0530

    test(unpredictable_gas_limit): add runtime typecheck with ajv

--- test/UNPREDICTABLE_GAS_LIMIT.test.ts
+++ test/UNPREDICTABLE_GAS_LIMIT.test.ts
@@ -4,14 +4,22 @@ import { Reverts } from '@sebastiantf/reverts/dist/types/Reverts';
 import { ethers } from 'ethers';
 import { parseEthersError } from '../src';
 import { EthersError } from '../src/types';
-import { REVERTS_ADDRESS, RPC_URL } from './utils';
+import { generateResultJSONSchema, REVERTS_ADDRESS, RPC_URL } from './utils';
+
+import Ajv, { ValidateFunction } from 'ajv';
+const ajv = new Ajv();
 
 let provider: ethers.providers.JsonRpcProvider;
 let reverts: Reverts;
+let validateResult: ValidateFunction;
 
 beforeAll(() => {
   provider = new ethers.providers.JsonRpcProvider(RPC_URL);
   reverts = Reverts__factory.connect(REVERTS_ADDRESS, provider);
+  // generate and compile JSON schema for Result
+  validateResult = ajv.compile(
+    JSON.parse(JSON.stringify(generateResultJSONSchema()))
+  );
 });
 
 describe(Logger.errors.UNPREDICTABLE_GAS_LIMIT, () => {
@@ -19,12 +27,17 @@ describe(Logger.errors.UNPREDICTABLE_GAS_LIMIT, () => {
     try {
       await reverts.estimateGas.requireWithMessage();
     } catch (error) {
+      expect(validateResult(error)).toEqual(false);
+
       const result = parseEthersError(error as EthersError);
 
       expect(result.reason).toEqual('Reverts: requireWithMessage');
       expect(result.code).toEqual(Logger.errors.UNPREDICTABLE_GAS_LIMIT);
       expect(result.parsedError).toBeDefined();
       expect(JSON.stringify(result.ethersError)).toEqual(JSON.stringify(error));
+
+      // Runtime typecheck validation with ajv
+      expect(validateResult(result)).toEqual(true);
     }
   });
 });
