cf947f154b32822c31929a529690c0780f26c3d6
Sebastian T F <sebastian.tharakan97@gmail.com>
2022-09-22 10:11:18 +0530

    feat(parse-ethers-error): add `parseUnpredictableGasLimitError()` to `parseEthersError()`

--- src/parseEthersError.ts
+++ src/parseEthersError.ts
@@ -1,3 +1,13 @@
-export function parseEthersError() {
-  return {};
+import { Logger } from '@ethersproject/logger';
+import { parseUnpredictableGasLimitError } from './lib/UNPREDICTABLE_GAS_LIMIT';
+import { EthersError, Result } from './types';
+
+export function parseEthersError(ethersError: EthersError): Result {
+  switch (ethersError.code) {
+    case Logger.errors.UNPREDICTABLE_GAS_LIMIT:
+      return parseUnpredictableGasLimitError(ethersError);
+
+    default:
+      throw new Error('Failed to parse ethers error');
+  }
 }
