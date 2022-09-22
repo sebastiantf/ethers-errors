17551da16a4d6fb11213e28e69dac048ef1b17db
Sebastian T F <sebastian.tharakan97@gmail.com>
2022-09-22 10:25:37 +0530

    test(utils): add `generateResultJSONSchema()` util

--- test/utils.ts
+++ test/utils.ts
@@ -1,3 +1,8 @@
+import { generateJSONSchema } from '../src/helpers';
+
 export const RPC_URL = 'https://rpc.ankr.com/eth_goerli';
 
 export const REVERTS_ADDRESS = '0x719f833CE1C1490fD1d773625d097654B6f82310';
+
+export const generateResultJSONSchema = () =>
+  generateJSONSchema('src/types.ts', 'Result');
