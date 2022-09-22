f7b4f2f1fe7cf136db01e8b4601cd463f7066470
Sebastian T F <sebastian.tharakan97@gmail.com>
2022-09-22 10:36:55 +0530

    feat(parse-ethers-error): add `parseCallExceptionError()` to `parseEthersError()`

--- src/parseEthersError.ts
+++ src/parseEthersError.ts
@@ -1,4 +1,5 @@
 import { Logger } from '@ethersproject/logger';
+import { parseCallExceptionError } from './lib/CALL_EXCEPTION';
 import { parseUnpredictableGasLimitError } from './lib/UNPREDICTABLE_GAS_LIMIT';
 import { EthersError, Result } from './types';
 
@@ -6,6 +7,8 @@ export function parseEthersError(ethersError: EthersError): Result {
   switch (ethersError.code) {
     case Logger.errors.UNPREDICTABLE_GAS_LIMIT:
       return parseUnpredictableGasLimitError(ethersError);
+    case Logger.errors.CALL_EXCEPTION:
+      return parseCallExceptionError(ethersError);
 
     default:
       throw new Error('Failed to parse ethers error');
